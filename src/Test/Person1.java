588f7549295c0aadf0bf1580e916c32c10fc6d1f
naruto <2045933252@qq.com>
2023-08-25 09:19:32 +0800

    对象在创建时，对象中静态代码块的代码会被执行 20230825

--- src/Test/Person1.java
+++ src/Test/Person1.java
@@ -12,5 +12,13 @@ public class Person1 {
         System.out.println(name);
     }
 
+    public static void main(String[] args) {
+
+        /**
+         * 在创建对象时，
+         * 静态代码块就运行了
+         */
+        Person1 person1 = new Person1();
+    }
 
 }
