ee0d501a16df161ad4e93e0a398cc1dc85b273e9
Naruto <2045933252@qq.com>
2024-04-11 14:04:12 +0800

    线程：多线程

---
+++ src/Thread/Demo01/MoreThread.java
@@ -0,0 +1,35 @@
+package Thread.Demo01;
+
+/**
+ * @Author
+ * @Date 2024/4/11 13:49
+ * @Description: 多线程：同时完成多项任务
+ */
+public class MoreThread {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(1);
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(2);
+            }
+        };
+
+        Thread thread3 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(3);
+            }
+        };
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
