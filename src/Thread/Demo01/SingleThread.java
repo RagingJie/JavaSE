c9d29f781272d320be98abb7ddc9c7e16a5c8a4e
Naruto <2045933252@qq.com>
2024-04-11 14:03:52 +0800

    线程：单线程

---
+++ src/Thread/Demo01/SingleThread.java
@@ -0,0 +1,14 @@
+package Thread.Demo01;
+
+/**
+ * @Author
+ * @Date 2024/4/11 13:48
+ * @Description: 单线程：按照顺序执行
+ */
+public class SingleThread {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
