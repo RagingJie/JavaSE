0f108ffdfea22fc36d6704d3c5f72180aa799e3a
Naruto <2045933252@qq.com>
2024-04-21 13:56:50 +0800

    线程：多线程

--- src/Thread/Demo01/MoreThread.java
+++ src/Thread/Demo01/MoreThread.java
@@ -28,8 +28,49 @@ public class MoreThread {
             }
         };
 
+        Thread thread4 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(4);
+            }
+        };
+
+        Thread thread5 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(5);
+            }
+        };
+
+        Thread thread6 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(6);
+            }
+        };
+
+        Thread thread7 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(7);
+            }
+        };
+
+        Thread thread8 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(8);
+            }
+        };
+
+
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
+        thread5.start();
+        thread6.start();
+        thread7.start();
+        thread8.start();
     }
 }
