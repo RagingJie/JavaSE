f2432a7330bf93c222b7b5d1007777375ebd90c8
Naruto <2045933252@qq.com>
2024-04-29 14:48:54 +0800

    完善http请求工具类

--- src/Http/HttpUtil/HttpRequest.java
+++ src/Http/HttpUtil/HttpRequest.java
@@ -1,8 +1,6 @@
 package Http.HttpUtil;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,27 +11,36 @@ import java.net.URL;
  */
 public abstract class HttpRequest {
 
+    /**
+     * @param url    请求路径
+     * @param params 携带参数
+     * @return java.lang.String
+     * @author Naruto
+     * @date 2024/4/29 14:12
+     * @description 发送get请求
+     */
     public static String executeGet(String url, String params) {
 
         String result = "";
 
         String urlName = url + params;
 
-        InputStreamReader inputStreamReader = null;
-        BufferedReader in = null;
         try {
             URL requestUrl = new URL(urlName);
             HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");  // application/json; charset=utf-8
-            inputStreamReader = new InputStreamReader(connection.getInputStream());
+            InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
 
-            in = new BufferedReader(inputStreamReader);
+            BufferedReader in = new BufferedReader(inputStreamReader);
 
             String line;
             while ((line = in.readLine()) != null) {
                 result = result + line;
             }
+
+            in.close();
+            inputStreamReader.close();
 
         } catch (Exception e) {
 //            e.printStackTrace();
@@ -42,17 +49,59 @@ public abstract class HttpRequest {
             } else {
                 System.out.println("get查询失败！！！" + e.getMessage());
             }
-        } finally {  // 关闭连接,释放资源
-            try {
-                in.close();
-                inputStreamReader.close();
-            } catch (IOException e) {
-                System.out.println("关闭资源失败，" + e.getMessage());
-//                e.printStackTrace();
-            }
         }
 
         return result;
 
+    }
+
+    /**
+     * @param url    请求路径
+     * @param params 携带参数
+     * @return java.lang.String
+     * @author Naruto
+     * @date 2024/4/29 14:13
+     * @description 发送post请求
+     */
+    public static String executePost(String url, String params) {
+
+        String result = "";
+        try {
+            URL requestUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+            connection.setDoOutput(true);   // 允许写出
+            connection.setDoOutput(true);   // 允许写入
+
+            OutputStreamWriter outWriter = new OutputStreamWriter(connection.getOutputStream());
+            outWriter.write(params);
+            outWriter.flush();
+
+            InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+
+            BufferedReader in = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+
+            in.close();
+            inputStreamReader.close();
+            outWriter.close();
+
+        } catch (Exception e) {
+//            e.printStackTrace();
+            if (e.getMessage().equals("Network is unreachable: connect")) {
+                System.out.println("请求post方法失败！！！,404，请求的《URL》地址不存在！！！");
+            } else {
+                System.out.println("请求post方法失败！！！");
+            }
+
+        }
+
+        return result;
     }
 }
