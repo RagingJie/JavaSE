54cdc1c560728e973ccebb02a8cd64bb9f9209be
Naruto <2045933252@qq.com>
2024-04-30 10:54:53 +0800

    测试启动类

---
+++ src/PuzzleGame/Test/TestApp.java
@@ -0,0 +1,14 @@
+package PuzzleGame.Test;
+
+/**
+ * @Author
+ * @Date 2024/3/13 9:30
+ * @Description:
+ */
+public class TestApp {
+    public static void main(String[] args) {
+//        new MyJFrame();
+
+        new MyJFrame01();
+    }
+}
