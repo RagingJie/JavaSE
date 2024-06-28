b093c7600bf71def91d16a05621c6a066eb5c310
Naruto <2045933252@qq.com>
2024-06-28 15:11:30 +0800

    将【时间戳】转换为【yyyy年MM月dd日 HH:mm:ss】时间格式

--- src/Utils/CommonUtil.java
+++ src/Utils/CommonUtil.java
@@ -1,10 +1,24 @@
 package Utils;
 
+import java.text.SimpleDateFormat;
+
 /**
  * @Author Naruto
  * @Date 2024/6/20 9:59
  * @Description:
  */
 public abstract class CommonUtil {
+
+    /**
+     * @param time 时间戳（毫秒值）
+     * @return java.lang.String
+     * @author Naruto
+     * @date 2024/6/28 15:09
+     * @description 将【时间戳】转换为【yyyy年MM月dd日 HH:mm:ss】时间格式
+     */
+    public static String dateFormat(Object time) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        return simpleDateFormat.format(time);
+    }
 
 }
