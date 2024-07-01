dac9181c106053d61607c403f7dd6b6990a93815
Naruto <2045933252@qq.com>
2024-07-01 14:39:33 +0800

    一次写一个字节数据

---
+++ src/IO_Stream/StudyDemo/FileOutputStream/FileOutputStreamDemo02.java
@@ -0,0 +1,29 @@
+package IO_Stream.StudyDemo.FileOutputStream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @Author
+ * @Date 2024/7/1 14:01
+ * @Description:
+ */
+public class FileOutputStreamDemo02 {
+
+    // void write(int b)                         一次写一个字节数据
+
+    public static void main(String[] args) throws IOException {
+
+        // 1.创建对象,会将文件种的内容清空
+        FileOutputStream fos = new FileOutputStream("src\\IO_Stream\\ExerciseFile\\a.txt");
+
+        // 2.写出数据
+        fos.write(49);
+        fos.write(50);
+        fos.write(51);
+        fos.write(52);
+
+        // 释放资源
+        fos.close();
+    }
+}
