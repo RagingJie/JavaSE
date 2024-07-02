0722676e72cb170a9e68e84ea1be1de159042a32
Naruto <2045933252@qq.com>
2024-07-03 00:16:51 +0800

    字节流读取中文会出现乱码

---
+++ src/IO_Stream/StudyDemo/Charset/CharsetDemo01.java
@@ -0,0 +1,26 @@
+package IO_Stream.StudyDemo.Charset;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @Author
+ * @Date 2024/7/3 0:11
+ * @Description:
+ */
+public class CharsetDemo01 {
+
+    /**
+     * 字节流读取中文会出现乱码
+     */
+
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("src\\IO_Stream\\ExerciseFile\\d.txt");
+
+        int val;
+        while ((val = fis.read()) != -1) {
+            System.out.print((char) val);
+        }
+    }
+}
