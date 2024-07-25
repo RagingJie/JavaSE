e778556554b103f9f6567d01e9c8b0d8cd630269
Naruto <2045933252@qq.com>
2024-07-25 15:16:39 +0800

    String的==比较

---
+++ src/Test/Tset09.java
@@ -0,0 +1,16 @@
+package Test;
+
+/**
+ * @Author
+ * @Date 2024/7/25 15:07
+ * @Description:
+ */
+public class Tset09 {
+    public static void main(String[] args) {
+        String a = new String("你好");
+        String b = "你好";
+        String c = "你好";
+        System.out.println(a == b);
+        System.out.println(b == c);
+    }
+}
