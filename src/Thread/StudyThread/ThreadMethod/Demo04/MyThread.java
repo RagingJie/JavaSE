90af198c6607b7f8b6182d11b385fdb5aaa5bc53
Naruto <2045933252@qq.com>
2024-07-30 23:48:57 +0800

    代码重构

--- src/Thread/StudyThread/ThreadMethod/Demo04/MyThread.java
+++ src/Thread/StudyThread/ThreadMethod/Demo04/MyThread.java
@@ -10,7 +10,7 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            System.out.println(Thread.currentThread().getName() + "@" + (i + 1));
+            System.out.println(getName() + "@" + (i + 1));
 
             // 表示出让当前CPU执行权
             Thread.yield();   // 是结果尽可能均匀一点
