b50ccba59b419ade4c167f2c81e9ba04a5fcd719
Naruto <2045933252@qq.com>
2024-08-02 16:29:13 +0800

    后续有时间再做这个练习（GUI编程）

---
+++ src/Thread/ExerciseDemo/Demo09/App.java
@@ -0,0 +1,16 @@
+package Thread.ExerciseDemo.Demo09;
+
+/**
+ * @Author
+ * @Date 2024/8/2 14:12
+ * @Description:
+ */
+public class App {
+    /*
+            给每日一记增加导出和导入的功能
+            提示：
+                 1、增加菜单，菜单中显示导出和导入功能
+                 2、导出：将所有日记文件打包成压缩包放到桌面上包名为data.zip
+                 3、导入：默认将桌面上的data.zip压缩包解压，获取里面所有的数据展示出来
+    */
+}
