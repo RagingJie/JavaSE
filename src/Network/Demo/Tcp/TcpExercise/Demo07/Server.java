02f053788d5548c5a89d36860ab1c1b49d7a21d0
Naruto <2045933252@qq.com>
2024-08-13 11:26:36 +0800

    综合练习：聊天室

---
+++ src/Network/Demo/Tcp/TcpExercise/Demo07/Server.java
@@ -0,0 +1,31 @@
+package Network.Demo.Tcp.TcpExercise.Demo07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @Author
+ * @Date 2024/8/13 10:46
+ * @Description: BS（接收浏览器的消息并打印）
+ */
+public class Server {
+    /*
+            客户端：不需要写
+            服务器：接收数据并打印
+    */
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8848);
+        Socket socket = serverSocket.accept();
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        int len;
+        while ((len = br.read()) != -1) {
+            System.out.print((char) len);
+        }
+
+        serverSocket.close();
+    }
+}
