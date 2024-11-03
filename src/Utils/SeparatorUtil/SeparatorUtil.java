074a10c6d61d00a60e86a2495e240ca48355a95a
Naruto <2045933252@qq.com>
2024-11-03 23:20:26 +0800

    分隔符工具类

---
+++ src/Utils/SeparatorUtil/SeparatorUtil.java
@@ -0,0 +1,19 @@
+package Utils.SeparatorUtil;
+
+/**
+ * @Author
+ * @Date 2024/11/3 23:18
+ * @Description:
+ */
+public abstract class SeparatorUtil {
+
+    /**
+     * @return java.lang.String
+     * @author Naruto
+     * @date 2024/11/3 23:20
+     * @description ===== 分隔符
+     */
+    public static String getSeparator() {
+        return "\n=======================================================================================================================================\n";
+    }
+}
